53e96ce53ce667c01b3a6cf590338fd1a79fb209
v-glb <v.networks@posteo.de>
2020-12-21 13:09:57 +0100

    Remove author name from footer

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -2,11 +2,10 @@ import React from "react"
 import { useStaticQuery, graphql } from "gatsby"
 
 export default () => {
-  const { author } = useStaticQuery(query).site.siteMetadata
   return (
     <div className="footer text-muted text-center">
       <span className="m-auto">
-        <b>{author}</b> &copy; {new Date().getFullYear()}. Made with&nbsp;
+        &copy; {new Date().getFullYear()}. Made with&nbsp;
         <a href="https://www.gatsbyjs.org/">Gatsby</a> &&nbsp;
         <span className="heart">&nbsp;❤&nbsp;</span>
       </span>
