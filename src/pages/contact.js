4d8d5326fb5f1054d48970d6b5b2aa6d0bd1beaa
v-glb <v.networks@posteo.de>
2020-12-22 15:14:12 +0100

    Add content to contact page

--- src/pages/contact.js
+++ src/pages/contact.js
@@ -1,6 +1,7 @@
 import React from "react"
 import { PageLayout, PageTitle } from "../components"
 import { SEO } from "../utils"
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome"
 import Container from "react-bootstrap/Container"
 
 export default () => {
@@ -8,8 +9,35 @@ export default () => {
     <PageLayout>
       <SEO title="Contact" />
       <PageTitle title="Contact"></PageTitle>
-      <Container className="mt-5 pt-3" fluid>
-        <h2>Contact me</h2>
+      <a href="../../v-glb-pubkey.asc" target="_blank" download>
+        <FontAwesomeIcon
+          style={{ fontSize: "2rem" }}
+          icon={["fas", "file-download"]}
+          className="icons file"
+        />
+      </a>
+      <Container>
+        <article className="w-75 m-auto pt-2 text-justify">
+          <p className="text-center">Download key</p>
+          <p className="i-5 mt-4 pt-2">
+            If you want to get in contact with me, feel free to grab my PGP-Key
+            and send me an email. Download the key either directly here or get
+            it from the&nbsp;
+            <a
+              href="https://keyserver.ubuntu.com/pks/lookup?search=v.glb%40posteo.de&fingerprint=on&op=index"
+              target="_blank"
+              rel="noopener noreferrer"
+            >
+              keyserver.
+            </a>
+          </p>
+          <br />
+          <p className="i-5">
+            <h5>
+              <b>v (dot) glb (at) posteo (dot) de.</b>
+            </h5>
+          </p>
+        </article>
       </Container>
     </PageLayout>
   )
