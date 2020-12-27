c3c2ea28623671b1da6092f2bf5af6c38a6242ac
v-glb <v.networks@posteo.de>
2020-12-27 15:02:36 +0100

    Fix default theme state

--- src/utils/theme.js
+++ src/utils/theme.js
@@ -1,8 +1,8 @@
 import React, { Component } from "react"
 
 const defaultState = {
-  dark: false,
-  toString: () => `light`,
+  dark: true,
+  toString: () => `dark`,
   toggleDark: () => {},
 }
 
