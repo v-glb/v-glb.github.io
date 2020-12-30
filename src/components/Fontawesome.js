763a65694453aff9411b53a1f7b7ed06f905da6b
v-glb <v.networks@posteo.de>
2020-12-30 20:48:32 +0100

    Only import specific icons to reduce bundle size

--- src/components/Fontawesome.js
+++ src/components/Fontawesome.js
@@ -1,4 +1,9 @@
 import { library } from "@fortawesome/fontawesome-svg-core"
-import { fab } from "@fortawesome/free-brands-svg-icons"
-import { fas } from "@fortawesome/free-solid-svg-icons"
-library.add(fab, fas)
+
+// Import only the specific Icons I need and use
+import { faVuejs } from "@fortawesome/free-brands-svg-icons"
+import { faGithub } from "@fortawesome/free-brands-svg-icons"
+import { faEnvelope } from "@fortawesome/free-solid-svg-icons"
+import { faFileDownload } from "@fortawesome/free-solid-svg-icons"
+
+library.add(faVuejs, faGithub, faEnvelope, faFileDownload)
